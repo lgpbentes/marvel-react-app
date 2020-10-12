b1c2544847777f51fab3f00e4f4498de19297e53
Larissa Bentes <lgpbentes@gmail.com>
2020-10-12 17:47:53 -0400

    feat: add loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+
+const Loading = () => {
+
+  return (
+    <div className="loader-wrapper">
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default Loading;
