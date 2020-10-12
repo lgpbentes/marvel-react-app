8d295ec9c1943913c3f789753de3560724a208f6
Larissa Bentes <lgpbentes@gmail.com>
2020-10-12 15:31:21 -0400

    Updates

---
+++ precache-manifest.461dd22f9e5321b3a66f97f94fb1fec8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77299b0a5d66533897e02c3317d74b3e",
+    "url": "/marvel-react-app/index.html"
+  },
+  {
+    "revision": "629aac9fd7a9684ae15b",
+    "url": "/marvel-react-app/static/css/main.66939bd9.chunk.css"
+  },
+  {
+    "revision": "f2f2116270d836038d60",
+    "url": "/marvel-react-app/static/js/2.1a8f7a33.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/marvel-react-app/static/js/2.1a8f7a33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629aac9fd7a9684ae15b",
+    "url": "/marvel-react-app/static/js/main.28bcf6bd.chunk.js"
+  },
+  {
+    "revision": "73f919c7ce87bb234cd8",
+    "url": "/marvel-react-app/static/js/runtime-main.2cf90057.js"
+  },
+  {
+    "revision": "32bf5cc4325bf1e5eea598fd547784d6",
+    "url": "/marvel-react-app/static/media/Group.32bf5cc4.svg"
+  },
+  {
+    "revision": "c435293bd07775bd62da4db4419ecc32",
+    "url": "/marvel-react-app/static/media/Group.c435293b.png"
+  },
+  {
+    "revision": "a196f94863ca8548c606ada8f2a7f030",
+    "url": "/marvel-react-app/static/media/shape.a196f948.svg"
+  }
+]);
