79e58c45e6df66ee283438b9fb7f700ade8cb443
Larissa Bentes <lgpbentes@gmail.com>
2020-10-14 11:53:09 -0400

    feat: add link to logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import logoImage from '../../assets/images/logo/MarvelLogo.svg';
 
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 interface LogoProps {
   styleType: string,
@@ -12,10 +13,12 @@ interface LogoProps {
 const Logo: React.FC<LogoProps> = ({ handleClick, styleType }) => {
 
   return (
-    <figure id="logo-wrapper" className={styleType ? styleType : 'default'}>
+    <Link to={'/'} style={{textDecoration: 'none'}}>
+      <figure id="logo-wrapper" className={styleType ? styleType : 'default'}>
         <img src={logoImage} alt="Marvel Search Heroes" />
         <h1>Search heros</h1>
     </figure>
+    </Link>
   );
 }
 
